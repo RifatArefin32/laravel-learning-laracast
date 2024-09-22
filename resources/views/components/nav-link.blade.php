98df7d07d3aa2dc912761a37913faf1052ec28c1
Rifat Arefin <arefinrifat786@gmail.com>
2024-09-22 22:27:30 +0600

    Update nav-link component

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -3,12 +3,11 @@
 @php
     $activeClass = $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white';
 @endphp
-<!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
 
 @if ($element_type === "a")
     <a {{ $attributes }} class="{{ $activeClass }} rounded-md px-3 py-2 text-sm font-medium text-white"
-        aria-current="page">{{ $slot }}</a>
+        aria-current="{{$active ? 'page' : 'false'}}">{{ $slot }}</a>
 @else
     <button {{ $attributes }} class="{{ $activeClass }} rounded-md px-3 py-2 text-sm font-medium text-white"
-        aria-current="page">{{ $slot }}</button>
+        aria-current="{{$active ? 'page' : 'false'}}">{{ $slot }}</button>
 @endif
