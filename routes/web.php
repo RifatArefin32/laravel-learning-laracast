a6986e3db6e8485ca318931807cabcd99d887179
Rifat Arefin <arefinrifat786@gmail.com>
2024-09-22 22:38:52 +0600

    Add data to Home view

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        'greeting' => 'Hello',
+        'username' => 'Rifat Arefin'
+    ]);
 });
 
 Route::get('/about', function() {
