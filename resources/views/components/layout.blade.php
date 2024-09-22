2a0c51526003420d9793c58af7b77f7893236f3b
Rifat Arefin <arefinrifat786@gmail.com>
2024-09-22 21:26:36 +0600

    Create basic layout components and reuse them

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> {{$titleText}} </title>
+</head>
+<body>
+    <div class="header">
+        <x-nav-link href="/">Home</x-nav-link>
+        <x-nav-link href="/about">About</x-nav-link>
+        <x-nav-link href="/contact">Contact</x-nav-link>
+    </div>
+    <div class="main-body">
+        {{$slot}}
+    </div>
+</body>
+</html>
