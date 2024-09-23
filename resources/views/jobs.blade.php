6115a4d173490f568a1275ddafda984592dbb703
Rifat Arefin <arefinrifat786@gmail.com>
2024-09-23 08:49:36 +0600

    Change the jobs page

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -6,7 +6,8 @@
         @foreach ($jobs as $job)
             <a href="/jobs/{{ $job['id'] }}">
                 <div class="text-blue-500 border-2 p-4 m-4 shadow-md rounded">
-                    {{ $job['title'] . ' | ' . $job['salary'] . ' ' . $job['currency'] . ' | See Details' }}
+                    <h1><strong>{{ $job['title'] }}</strong></h1>
+                    <h1>{{ $job['salary'] . ' ' . $job['currency'] }} per month | {{ $job->employer->name }}</h1>
                 </div>
             </a>
         @endforeach
